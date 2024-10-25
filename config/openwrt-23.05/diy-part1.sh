c7d133a5d2f48a2ec10f235b9871366ab741750c
virgil141 <virgilxiao141@gmail.com>
2024-10-25 10:10:29 +0800

    add official branch 23.05

---
+++ config/openwrt-23.05/diy-part1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#========================================================================================================================
+# https://github.com/ophub/amlogic-s9xxx-openwrt
+# Description: Automatically Build OpenWrt
+# Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
+# Source code repository: https://github.com/openwrt/openwrt / Branch: main
+#========================================================================================================================
+
+# Add a feed source
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+# other
+# rm -rf package/utils/{ucode,fbtest}
+
